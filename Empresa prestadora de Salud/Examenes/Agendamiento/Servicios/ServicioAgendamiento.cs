178837c00a9f42854b0679d5cfd800b22f2396d9
John Alexander Peñaloza Rojas <rojas.jhalex@gmail.com>
2025-10-05 09:30:29 -0500

    Creation of the second service called scheduling
    Creation of the second service called scheduling

---
+++ Empresa prestadora de Salud/Examenes/Agendamiento/Servicios/ServicioAgendamiento.cs
@@ -0,0 +1,20 @@
+﻿using Agendamiento.ServiceBus;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agendamiento.Servicios
+{
+    public class ServicioAgendamiento
+    {
+        public void AgendarCita(string mensaje)
+        {
+            Console.WriteLine("Procesando agendamiento...\n");
+            Console.WriteLine($"Mensaje recibido: {mensaje}\n");
+            Console.WriteLine("Cita para lectura de resultados ha sido agendada correctamente.\n");
+        }
+    }
+}
