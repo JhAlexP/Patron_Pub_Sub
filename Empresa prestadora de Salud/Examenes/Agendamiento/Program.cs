5c71a20ef3fbcba3d70409c3ad3286eb02565782
John Alexander Peñaloza Rojas <lgc90smart@gmail.com>
2025-10-05 09:36:05 -0500

    Create service Agendamiento
    Create service Agendamiento

---
+++ Empresa prestadora de Salud/Examenes/Agendamiento/Program.cs
@@ -0,0 +1,13 @@
+﻿using Agendamiento.ServiceBus;
+using Agendamiento.Servicios;
+using Microsoft.Extensions.Configuration;
+
+var config = new ConfigurationBuilder()
+    .AddUserSecrets<Program>() // 🔐 Lee secrets
+    .Build();
+
+var agendamientoService = new ServicioAgendamiento();
+var subscriber = new ServiceBusSubscriber(config, agendamientoService);
+
+await subscriber.EscucharAsync();
+
